40f8d2dd04ec0812712ecc1462a02f7fcd92a78f
haduchau321 <83698436+haduchau321@users.noreply.github.com>
2021-05-09 00:01:47 +0800

    Delete info.py

--- info.py
+++
@@ -1,8 +0,0 @@
-import requests
-from ast import literal_eval
-
-def get_out():
-    dataName = literal_eval(requests.get('http://ip-api.com/json').text)
-    ip = dataName['query']
-    vitri = dataName['regionName']
-    return ip,vitri
