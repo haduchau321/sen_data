53605fd34ab541125d66734e53bbbd116629d89a
haduchau321 <83698436+haduchau321@users.noreply.github.com>
2021-05-09 00:11:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 import author
 
 key = 'haduchau_admin'
@@ -7,7 +6,6 @@ app = FastAPI()
 
 @app.get('/send/{ip}/{vitri}/{data}')
 async def send(ip:str,vitri:str,data:str):
-    print(ip,vitri,data)
     author.send(ip,vitri,data)
     return True
 
@@ -17,6 +15,6 @@ async def send(api_key:str):
         out = author.get_data()
         return out
 
-uvicorn.run(app)
+
 
 #  {"id_sy":"AQE5yR7GMR_37vQ:AQFZ_gcVHE0wO-M","id_post":"22412"}
