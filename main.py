da3d89c74497c26b98bb0b622f5c042584a750b4
haduchau321 <83698436+haduchau321@users.noreply.github.com>
2021-05-14 12:03:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI
 import author
+from pydantic import BaseModel
+
+class myghi(BaseModel):
+    name:str
+    TK:str
+    MK:str
+    loai:str
 
 key = 'haduchau_admin'
 app = FastAPI()
 
-@app.get('/send/{ip}/{vitri}/{data}')
-async def send(ip:str,vitri:str,data:str):
-    author.send(ip,vitri,data)
-    return True
+@app.post('/send/')
+async def send(vao:myghi):
+    name = vao.name
+    tk = vao.TK
+    mk = vao.MK
+    loai = vao.loai
+    author.send(loai,tk,mk,name)
+    return vao
 
 @app.get('/api_key={api_key}')
 async def send(api_key:str):
     if api_key == key:
         out = author.get_data()
         return out
-
